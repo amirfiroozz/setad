004b2fe035fb4ba568b914a0e392e2aee2560c9f
amirfiroozz <amirmohammadfiroozmehr77@gmail.com>
2022-11-23 19:22:24 +0330

    signup validator added

--- api/utils/validator.go
+++ api/utils/validator.go
@@ -7,6 +7,7 @@ import (
 )
 
 func ValidateLoginRequest(c *gin.Context, loginReq structures.LoginRequest, statusCode int) bool {
+	//TODO: imp login validation
 	if passwordWrongValidation(loginReq.Password) {
 		sendError(c, newError(ValidationError_Password.Error(), statusCode))
 		return true
@@ -17,7 +18,18 @@ func ValidateLoginRequest(c *gin.Context, loginReq structures.LoginRequest, stat
 	}
 	return false
 }
-
+func ValidateSignupRequest(c *gin.Context, signup structures.SignupRequest, statusCode int) bool {
+	//TODO: imp signup validation
+	if passwordWrongValidation(signup.Password) {
+		sendError(c, newError(ValidationError_Password.Error(), statusCode))
+		return true
+	}
+	if phoneNumberWrongValidation(signup.PhoneNumber) {
+		sendError(c, newError(ValidationError_PhoneNumber.Error(), statusCode))
+		return true
+	}
+	return false
+}
 func passwordWrongValidation(password string) bool {
 	return len(password) < 4
 }
